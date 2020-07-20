c0092e6bd1ac1dce06e79f42318133c1086b39bc
Joseph Miller <joem@breachintelligence.com>
2020-07-20 15:48:30 -0400

    Updated license, and defaults for config.js file.

--- config/config.js
+++ config/config.js
@@ -59,16 +59,16 @@ module.exports = {
   },
   request: {
     // Provide the path to your certFile. Leave an empty string to ignore this option.
-    // Relative paths are relative to the METD integration's root directory
+    // Relative paths are relative to the Greynoise integration's root directory
     cert: '',
     // Provide the path to your private key. Leave an empty string to ignore this option.
-    // Relative paths are relative to the METD integration's root directory
+    // Relative paths are relative to the Greynoise integration's root directory
     key: '',
     // Provide the key passphrase if required.  Leave an empty string to ignore this option.
-    // Relative paths are relative to the METD integration's root directory
+    // Relative paths are relative to the Greynoise integration's root directory
     passphrase: '',
     // Provide the Certificate Authority. Leave an empty string to ignore this option.
-    // Relative paths are relative to the METD integration's root directory
+    // Relative paths are relative to the Greynoise integration's root directory
     ca: '',
     // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
     // the url parameter (by embedding the auth info in the uri)
@@ -114,8 +114,8 @@ module.exports = {
         'If set to true, IPs that have not been seen by Greynoise will not be displayed in the Polarity Overlay window.',
       default: false,
       type: 'boolean',
-      userCanEdit: false,
-      adminOnly: true
+      userCanEdit: true,
+      adminOnly: false
     }
   ]
 };
